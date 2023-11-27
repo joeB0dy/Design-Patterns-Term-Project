bcfd4ec55c3d55a2c3fd7b93fe6becde35a457b0
joeB0dy <sjose@umich.edu>
2023-11-26 21:04:30 -0500

    hosted database working.

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "sql5.freemysqlhosting.net",
     user: 'sql5665289',
     password: 'P9jeb9GNuF',
     database: 'sql5665289'
 
 })
 
-app.get('/', (re, res)=> {
-    return res.json("From backend");
-})
+// Endpoint to get data
+app.get('/api/data', (req, res) => {
+    // Query your database and send data
+    db.query('SELECT * FROM Users', (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
 
 app.listen(8081, ()=>{
     console.log("listening");
--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./Pages/Login/login";
 import MainPage from "./Pages/MainPage/main";
 import SignUp from "./Pages/SignUp/SignUp"
+import React, { useState, useEffect } from 'react';
+
+
 export default function App() {
   const navigate = useNavigate();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from Express server
+    fetch('http://localhost:8081/api/data')
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(err => console.error("Error fetching data", err));
+});
 
   return (
     <div className="App">
@@ -12,7 +24,11 @@ export default function App() {
       <h1>MyPass Password Master System</h1>
       <button onClick={() => navigate("/login")}>Login Page </button>
       <button onClick={() => navigate("/")}>Home </button>
-     
+      <div>
+            <h1>Data from Server</h1>
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+        </div>
+        
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<MainPage />} />
